6b12d504344814840a5ec4495e96a467b2a25b19
HARRISH2005 <harrishd650@gmail.com>
2026-02-27 15:04:46 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,4 +1,4 @@
-
+//UC1
 public class PalindromeChecker {
 
     public static void main(String[] args) {
@@ -10,5 +10,28 @@ public class PalindromeChecker {
         System.out.println("System initialized successfully.");
         System.out.println();
         System.out.println("Ready to proceed to next use case...");
+    }
+}
+
+//UC2
+    public static void main(String[] args) {
+        String input = "madam";
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input String : " + input);
+        if (isPalindrome) {
+            System.out.println("Result : The string is a Palindrome.");
+        } else {
+            System.out.println("Result : The string is NOT a Palindrome.");
+        }
+
+        System.out.println("Program completed.");
     }
 }
